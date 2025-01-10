3e30c269ca09ddecee3009feb0f4caef364dab9a
gilrallo <gil.rallo01@gmail.com>
2025-01-10 13:02:40 +0100

    users and user details urls and views with simple html

--- bot/urls.py
+++ bot/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = "bot"
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.redirect_view),
+    # ex: /bot/users
+    path("users/", views.users, name="users"),
+    # ex: /bot/users/5
+    path("users/<int:user_id>/", views.user_details, name="user_details"),
+
 ]
--- bot/views.py
+++ bot/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import Http404
 from django.http import HttpResponse
 
-# Create your views here.
+from bot.models import FoodCategory, Food, User
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the bot index.")
+def redirect_view(request):
+    """
+    View to redirect to another url.
+    This view is called when the url is "/bot". So being in "/bot" will redirect you to "/bot/users/"
+    """
+    response = redirect('/bot/users/')
+    return response
+
+def users(request):
+    """
+    View that shows a list of all the users
+    """
+    current_users = User.objects.all()
+    context = {
+        "current_users": current_users,
+    }
+    return render(request, "bot/users.html", context)
+
+
+def user_details(request, user_id):
+    """
+    View that shows the details of a certain user
+    """
+    user = get_object_or_404(User, pk=user_id)
+    return render(request, "bot/user_details.html", {"user": user})
