55ab119ac43725507a73d9f43bf38c4c324ace9b
gilrallo <gil.rallo01@gmail.com>
2025-01-13 11:04:06 +0100

    chat working and given nutrition users view
    disabling register user button if name == ""

--- bot/urls.py
+++ bot/urls.py
@@ -6,31 +6,33 @@ app_name = "bot"
 urlpatterns = [
 
     # ---------------------- PAGES ----------------------
-    # ex: /bot
+    # ex: /bot/
     path("", views.redirect_view),
 
-    # ex: /bot/simulate
-    path("simulate/", views.simulate, name="simulate"),
+    # ex: /bot/simulate/
+    path("simulate/", views.simulate_conversations, name="simulate"),
 
-    # ex: /bot/register
+    # ex: /bot/register/
     path("register/", views.user_registration, name="user_registration"),
-    # ex: /bot/chat
+    # ex: /bot/chat/5/
     path("chat/<int:user_id>/", views.conversation, name="conversation"),
 
-    # ex: /bot/users
+    # ex: /bot/users/
     path("users/", views.users, name="users"),
-    # ex: /bot/users/5
+    # ex: /bot/users/5/
     path("users/<int:user_id>/", views.user_details, name="user_details"),
+    # ex: /bot/users/nutrition/vegetarian/
+    path("users/<str:nutrition>/", views.nutrition_users, name="nutrition_users"),
 
     # ---------------------- ACTIONS ----------------------
     
-    # ex: /bot/simulate_conversations
+    # ex: /bot/simulate_conversations/
     path("simulate_conversations/", views.simulate_conversations, name="simulate_conversations"),
     
-    # ex: /bot/create_user
-    path("start_conversation/<str:name>/", views.start_conversation, name="start_conversation"),
-    # TODO
-    # ex: /bot/response
-    path("response/", views.response, name="response"),
+    # ex: /bot/start_conversation/
+    path("start_conversation/", views.start_conversation, name="start_conversation"),
+
+    # ex: /bot/response/5/
+    path("response/<int:user_id>/", views.response, name="response"),
     
 ]
--- bot/views.py
+++ bot/views.py
@@ -19,7 +19,7 @@ def redirect_view(request):
     return response
 
 
-def simulate(request):
+def simulate_conversations(request):
     """
     View that renders the page with a button to simulate 100 conversations
     """
@@ -32,16 +32,17 @@ def user_registration(request):
     """
     return render(request, "bot/register.html")
 
-# TODO
+
 def conversation(request, user_id):
     """
-    View that renders the page of the conversation with the chatbot
+    View that renders the page of the conversation with the chatbot.
+    Obtains all the messages form the user with the id sent as a parameter and they will be displayed in the page.
     """
 
     # Getting the messages from the user passed as parameter
     messages = list(Message.objects.filter(user__id = user_id))
 
-    return render(request, "bot/chat.html", {"messages" : messages})
+    return render(request, "bot/chat.html", {"user_id" : user_id, "messages" : messages})
 
 
 def users(request):
@@ -51,13 +52,26 @@ def users(request):
     current_users = User.objects.all()
     context = {
         "current_users": current_users,
+        "nutrition": "",
+    }
+    return render(request, "bot/users.html", context)
+
+
+def nutrition_users(request, nutrition):
+    """
+    View that shows a list of all the users with a the nutrition given as a parameter
+    """
+    current_users = User.objects.filter(nutrition__name=nutrition)
+    context = {
+        "current_users": current_users,
+        "nutrition": nutrition,
     }
     return render(request, "bot/users.html", context)
 
 
 def user_details(request, user_id):
     """
-    View that shows the details of a certain user
+    View that shows the details of the user with the is passed as a parameter
     """
     user = get_object_or_404(User, pk=user_id)
     return render(request, "bot/user_details.html", {"user": user})
@@ -78,26 +92,24 @@ def simulate_conversations(request):
 
         # obtaining and storing question about his/her top 3 favorite foods question
         favorite_foods_question = favorite_food_question_prompt(name=user_name)
-        print(favorite_foods_question)
         Message.objects.create(user=user_obj, conversation_stage=Message.CONVERSATION_QUESTIONS["2"], owner=Message.OWNER["BOT"], message=favorite_foods_question)
 
         # obtaining and storing user answer
         user_answer = favorite_food_answer_prompt()
-        print(user_answer)
         Message.objects.create(user=user_obj, conversation_stage=Message.CONVERSATION_QUESTIONS["2"], owner=Message.OWNER["USR"], message=user_answer)
 
         # identify and storing foods in user answer
         foods_json = identify_food_prompt(message=user_answer)
-        print(foods_json)
         for food in json.loads(foods_json)["foods"]:
             food_obj, created = Food.objects.get_or_create(name=food)
+
+            # it could be that the food had already been mentioned before by another user, in that case we already have its category
             if created:
                 category = identify_food_category_prompt(food=food_obj.name)
-                print(food_obj, category)
                 food_obj.category = FoodCategory.objects.get(name=category)
                 food_obj.save()
 
-            # Adding the Food object in the ManyToMany field
+            # Adding the Food as a favorite one of the user (adding object in the ManyToMany field)
             user_obj.favorite_foods.add(food_obj)
         
         # Calculating the nutrition category of the user based on his/her top 3 favorite foods
@@ -107,15 +119,18 @@ def simulate_conversations(request):
     return HttpResponseRedirect(reverse("bot:users"))
 
 
-def start_conversation(request, name):
+def start_conversation(request):
     """
     View that initiates a conversation: 
         1. creates a user
-        2. creates the first message
+        2. creates the first question from the bot
     It is called when the button in the user_registration is pressed
     """
+    # we get the userName from the form input divs, using the name parameter values to access them
+    user_name = request.POST['userName']
+
     # create a user
-    user = User.objects.create(name=name)
+    user = User.objects.create(name=user_name)
 
     # obtaining and storing question about the user top 3 favorite foods question
     favorite_foods_question = favorite_food_question_prompt(name=user.name)
@@ -123,9 +138,46 @@ def start_conversation(request, name):
 
     return HttpResponseRedirect(reverse("bot:conversation", args=(user.id,)))
 
-# TODO
-def response(request):
+
+def response(request, user_id):
     """
+    View that processes the user message and gives and answer
+        1. Gets the user message from the POST dictionary
+        2. Creates a Message object to store the message
+        3. Identify the foods mentioned in the message
+        4. Creates a Food object and obtains its category if the food hadn't been mentioned before by another user
+        5. Adds the food to the favorite foods of the user
+        6. Calculates the user nutrition type
+        7. Stores a message with the answer from the bot
+
+    It is called when the user sends a message
+    """
+
+    # we get the message from the form input divs, using the name parameter values to access them
+    use_message = request.POST['message']
+    user = User.objects.get(pk=user_id)
+
+    Message.objects.create(user=user, conversation_stage=Message.CONVERSATION_QUESTIONS["2"], owner=Message.OWNER["USR"], message=use_message)
+
+    # identify and storing foods in user answer
+    foods_json = identify_food_prompt(message=use_message)
+
+    for food in json.loads(foods_json)["foods"]:
+        food_obj, created = Food.objects.get_or_create(name=food)
+        if created:
+            category = identify_food_category_prompt(food=food_obj.name)
+
+            food_obj.category = FoodCategory.objects.get(name=category)
+            food_obj.save()
+
+        # Adding the Food object in the ManyToMany field
+        user.favorite_foods.add(food_obj)
     
-    """
-    return 0
+    # Calculating the nutrition category of the user based on his/her top 3 favorite foods
+    user.calculate_nutrition()
+    user.save()
+
+    bot_answer = f"It looks like you are {user.nutrition}!"
+    Message.objects.create(user=user, conversation_stage=Message.CONVERSATION_QUESTIONS["2"], owner=Message.OWNER["BOT"], message=bot_answer)
+
+    return HttpResponseRedirect(reverse("bot:conversation", args=(user.id,)))
