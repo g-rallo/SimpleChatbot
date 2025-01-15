8f7474b577df375eff42874b372e57a8cf89fe65
gilrallo <gil.rallo01@gmail.com>
2025-01-15 16:09:08 +0100

    debug = False

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = ['simplechatbot-h3e2ceeyefgxhpcf.uksouth-01.azurewebsites.net', '127.0.0.1']
 
